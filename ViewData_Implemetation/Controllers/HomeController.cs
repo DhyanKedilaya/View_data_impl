6dbb62d737f3cf43104cafbeb2af16400008874a
DhyanKedilaya <84610613+DhyanKedilaya@users.noreply.github.com>
2024-07-23 00:52:37 +0530

    Add files via upload

---
+++ ViewData_Implemetation/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        ViewData["Countries"] = new List<string>{
+            "India",
+            "Malaysia",
+            "Dubai",
+            "USA",
+            "UK"
+         };
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
